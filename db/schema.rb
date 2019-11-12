de688f99d37a534ab0757c8fd4dd0af35836d986
Jesse <jesse.lee.smith@gmail.com>
2019-11-12 11:11:52 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,22 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
+
 end
